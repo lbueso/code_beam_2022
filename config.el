7fae6a59da95a763d6cc48a3867e52dea3bc8c01
Luis Eduardo Bueso de Barrio <luiseduardobueso96@gmail.com>
2022-05-18 00:28:07 +0200

    update

--- config.el
+++ config.el
@@ -5,4 +5,14 @@
 (add-to-list 'org-latex-packages-alist '("" "listings"))
 (add-to-list 'org-latex-packages-alist '("" "color"))
 
+(defun org-latex-code (code _contents info)
+  "Transcode a CODE object from Org to LaTeX.
+CONTENTS is nil.  INFO is a plist used as a communication
+channel."
+  (format "\\lstinline[language=%s, style=display]~%s~"
+          my-default-code-markup-language
+          (org-element-property :value code)))
+
+(setq my-default-code-markup-language "elixir")
+
 (defun compile-org-in-background () (start-process "org-compile" "*org-compile*" "make"))
